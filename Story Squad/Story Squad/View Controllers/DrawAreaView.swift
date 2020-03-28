d8212040df59a8c1f53f53c079d6bc0e3a83a166
jonalynn masters <jonalynna.masters@gmail.com>
2020-03-28 16:01:40 -0700

    DrawUI reconfig separate view

---
+++ Story Squad/Story Squad/View Controllers/DrawAreaView.swift
@@ -0,0 +1,21 @@
+//
+//  DrawAreaView.swift
+//  Story Squad
+//
+//  Created by Jonalynn Masters on 3/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class DrawAreaView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
